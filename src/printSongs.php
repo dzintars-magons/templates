e6f4218f95487ebfeefae657e00adba6b39a507e
dzintars-magons <dzintars.magons@gmail.com>
2020-01-18 17:28:12 +0200

    added style

--- src/printSongs.php
+++ src/printSongs.php
@@ -12,20 +12,24 @@ $isFetchModeSet = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $allRows = $stmt->fetchAll();
 
 //SOME SORT OF TRICK where all columns will be printed
+//finally we can print the results
+echo "<div class='song-container'>";
 $columnsPrinted = false; 
 foreach ($allRows as $row){
     if (!$columnsPrinted){
+        echo "<div class='row-column-names'>";
         foreach ($row as $key => $value){
-            echo "<span class='column-name'>KEY: $key </span>";
+            echo "<span class='column-name'>$key </span>";
         }
         $columnsPrinted = true;
-        echo "<hr>";
+        echo "</div>";
     }
-    echo "<div>";
+    echo "<div class='row-song'>";
     // echo "<span>Title: " .$row["title"] . "</span>";
     foreach ($row as $key => $value){
         echo "<span class='value-cell'>$value</span>";
     }
     echo "</div>";
 }
+echo "</div>";
 echo "<hr>";
