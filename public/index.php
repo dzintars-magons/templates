f342e37227d2e0bf307f6f39148fa2afeba7fcad
dzintars-magons <dzintars.magons@gmail.com>
2020-01-18 17:40:38 +0200

    added form for adding songs to DB

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require_once '../src/templates/head.php';
 
+require_once '../src/addSongs.php';
+
 require_once '../src/printSongs.php';
 
 require_once '../src/templates/foot.php';
---
+++ src/addSongs.php
@@ -0,0 +1,8 @@
+<div class="song-form">
+<form action="addSongs.php" method="post">
+    <input name="title" placeholder="enter song title" required>
+    <input name="artist" placeholder="enter artist">
+    <input type="number" name="length" id="" min="1" max="10000" value="180">
+    <button type="submit">Add New Song</button>
+</form>
+</div>
