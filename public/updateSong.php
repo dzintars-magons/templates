ddacaeb7c29252ac11fd1c7b99fef65ef8c57d4d
dzintars-magons <dzintars.magons@gmail.com>
2020-01-19 16:09:13 +0200

    minor corrections

--- public/updateSong.php
+++ public/updateSong.php
@@ -1,2 +1,32 @@
 <?php
-    
+    require_once '../src/db.php';
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        //printSongs.php button has name='delete' and form has method='post' ofcourse
+        $song_id = $_POST['update'];
+        $title = $_POST['title'];
+        $artist = $_POST['artist'];
+        $length = $_POST['length'];
+        // var_dump($_POST);
+        // die("Got song_id $song_id");
+
+        // prepare and bind
+        $stmt = $conn->prepare("UPDATE `tracks` 
+            SET `title` = (:title),
+                `artist` = (:artist),
+                `length` = (:length)
+            WHERE `tracks`.`id` = (:songid)");
+
+        $stmt->bindParam(':songid', $song_id);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':artist', $artist);
+        $stmt->bindParam(':length', $length);
+        
+        $stmt->execute();
+        //we go to our index.php
+        header('Location: /');
+
+    } else {
+        echo "That was not a POST, most likely GET";
+    }
+    // die("Let's add some songs!");
