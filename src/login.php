9976bf6b2ba2ed4e9cdf751284f61d054a450a42
dzintars-magons <dzintars.magons@gmail.com>
2020-01-20 11:09:15 +0200

    creating login - second commit

--- src/login.php
+++ src/login.php
@@ -4,7 +4,13 @@
     if (isset($_SESSION['username'])) {
         echo "You are logged in " . $_SESSION['username'];
     } else { 
-        echo "You need to register";
-        echo "<a href='register.php'>Register</a>";
+        echo "<div class='register-p'>You need to <a href='register.php'>Register</a>or ";
+        echo "<form class='login-f' action='processLogin.php' method='post'>";
+        echo "<input name='username' placeholder='Enter username'>";
+        echo "<input name='password' type='password' placeholder='Enter password'>";
+        echo "<button>Login</button>";
+        echo "</form>";
+        echo "</div>";
+
     }
     echo "<hr>";
