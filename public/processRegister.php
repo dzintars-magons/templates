729a35008d6d6c98d9e524771b45cd7924d1e642
dzintars-magons <dzintars.magons@gmail.com>
2020-01-20 16:36:26 +0200

    minor correction

---
+++ public/processRegister.php
@@ -0,0 +1,35 @@
+<?php
+    require_once '../src/db.php';
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // echo "We got a POST request!<br>";
+        // foreach ($_POST as $key => $value) {
+        //     echo "We received name $key with value $value <br>";
+        // }
+        // if (isset($_POST['myname'])) {
+        //     echo "Why hello there " . $_POST['myname'] . "! <hr>";
+        //     $_SESSION['myname'] = $_POST['myname'];
+        // }
+        // var_dump ($_POST);
+
+        //we need to add song to database
+        $title = $_POST['title'];
+        $artist = $_POST['artist'];
+        $length = $_POST['length'];
+        $user_id = 1; //TODO add real users
+
+        // prepare and bind
+        $stmt = $conn->prepare("INSERT INTO tracks (title, artist, length, user_id) 
+                                VALUES (:title, :artist, :length, :user_id)");
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':artist', $artist);
+        $stmt->bindParam(':length', $length);
+        $stmt->bindParam(':user_id', $user_id);
+        
+        $stmt->execute();
+        //we go to our index.php
+        header('Location: /');
+
+    } else {
+        echo "That was not a POST, most likely GET";
+    }
+    // die("Let's add some songs!");
