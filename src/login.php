3dafd2f5411facb84d13d4d6200c1a7ab3270fdf
dzintars-magons <dzintars.magons@gmail.com>
2020-01-21 11:01:18 +0200

    started creating logout - first - form

--- src/login.php
+++ src/login.php
@@ -3,6 +3,9 @@
     session_start();
     if (isset($_SESSION['username'])) {
         echo "You are logged in " . $_SESSION['username'];
+        echo "<form action='processLogout.php' method='post' >";
+        echo "<button>Logout</button>";
+        echo "</form>";
     } else { 
         echo "<div class='register-p'>You need to <a href='register.php'>Register</a>or ";
         echo "<form class='login-f' action='processLogin.php' method='post'>";
