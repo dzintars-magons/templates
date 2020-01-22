1f8e41fca2689eca49d409cf705324656d30014d
dzintars-magons <dzintars.magons@gmail.com>
2020-01-22 19:10:06 +0200

    created function for process login

--- public/processLogin.php
+++ public/processLogin.php
@@ -1,10 +1,12 @@
 <?php
     session_start();
     require_once '../src/db.php';
-    require_once '../src/dbutils.php';
+
+    $conn = getConn($SERVER, $DB, USER, PW);
     
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $username = $_POST['username'];
         $password = $_POST['password'];
         checkLogin($conn, $username, $password);
     }
+    
--- src/dbutils.php
+++ src/dbutils.php
@@ -1,4 +1,19 @@
 <?php
+    require_once '../config/config.php';
+
+    function getConn($server, $db, $user, $pw, $port = 3306) {
+        try {
+            //if we need to set port it would come after $SERVER; port=3307;dbname=$DB
+            $conn = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pw);
+            //set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            $conn = null;
+        }
+        return $conn;
+    }
+
     function checkLogin($conn, $username, $password) 
     {
 
